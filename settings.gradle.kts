a3dace2fccd95c2bdf39c08578ae3a2c4ee668c4
손찬형 <sonchanhyeong710@gmail.com>
2025-06-10 18:05:07 +0900

    :heavy_plus_sign: Add include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,12 +22,15 @@ dependencyResolutionManagement {
 rootProject.name = "Sumory"
 include(":app")
 
+include(":core")
 include(":core:design")
 include(":core:model")
 include(":core:ui")
 
+include(":data")
 include(":data:auth")
 include(":data:diary")
 
+include(":domain")
 include(":domain:auth")
 include(":domain:diary")
