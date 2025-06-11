0ae5532af84c5c86100f49062c7e5c5b1db69fd2
손찬형 <sonchanhyeong710@gmail.com>
2025-06-11 19:40:05 +0900

    :sparkles: :: create AndroidCompose.kt

---
+++ build-logic/convention/src/main/java/com/sonchan/convention/AndroidCompose.kt
@@ -0,0 +1,17 @@
+package com.sonchan.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+
+internal fun Project.configureAndroidCompose(
+    commonExtension: CommonExtension<*, *, *, *, *>
+) {
+
+    commonExtension.apply {
+        buildFeatures.compose = true
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
+        }
+    }
+}
