9cf857c723d789a9007f4747cc34288c77d4ee35
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 15:37:30 +0900

    :sparkles: :: Create SumoryTypography

---
+++ core/design-system/src/main/java/com/sonchan/design_system/theme/SumoryTheme.kt
@@ -0,0 +1,13 @@
+package com.sonchan.design_system.theme
+
+import androidx.compose.runtime.Composable
+import com.sonchan.design_system.theme.color.SumoryColor
+
+@Composable
+fun SumoryTheme(
+    colors: SumoryColor = SumoryColor,
+    typogrphy: SumoryTypography = SumoryTy,
+
+){
+
+}
