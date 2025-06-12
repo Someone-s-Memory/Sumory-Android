77e9c4f2a666eb7aec12c6c7817e1f974ca2ba48
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 10:13:15 +0900

    :memo: :: change build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,33 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("sumory.android.application")
+    id("sumory.android.hilt")
 }
 
 android {
     namespace = "com.sonchan.sumory"
     compileSdk = 35
 
-    defaultConfig {
-        applicationId = "com.sonchan.sumory"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/DEPENDENCIES"
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
     }
 }
 
 dependencies {
+    // todo : Add Other Project Implementation -> ex) implementation(project(":core:___")) / (project(":feature:____"))
+    implementation(project(":data:auth"))
+    implementation(project(":data:diary"))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(project(":domain:auth"))
+    implementation(project(":domain:diary"))
+    implementation(project(":core:design-system"))
+
+    implementation(project(":core:model"))
+    implementation(project(":core:ui"))
+
+    implementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    implementation(libs.app.update.ktx)
 }
