e327d8e14e6bcdd9c0d1daf75b38e499d85d63e0
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 10:14:50 +0900

    :bug: :: fix wrong namesapce

--- build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
@@ -13,7 +13,7 @@ class JvmLibraryConventionPlugin : Plugin<Project> {
             // Apply necessary plugins for JVM libraries
             with(pluginManager) {
                 apply("org.jetbrains.kotlin.jvm")  // Apply Kotlin JVM plugin for Kotlin support in JVM projects
-                apply("jusicool.android.lint")  // Apply Android Lint plugin for code quality checks (custom plugin)
+                apply("sumory.android.lint")  // Apply Android Lint plugin for code quality checks (custom plugin)
                 apply("com.google.devtools.ksp")  // Apply KSP (Kotlin Symbol Processing) plugin for code generation
             }
 
