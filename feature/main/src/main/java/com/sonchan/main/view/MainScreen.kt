71dff14e4e9fd951a2b10c30e1838c980d15738d
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 15:46:33 +0900

    :memo: :: Add SumoryTheme

--- feature/main/src/main/java/com/sonchan/main/view/MainScreen.kt
+++ feature/main/src/main/java/com/sonchan/main/view/MainScreen.kt
@@ -1,8 +1,18 @@
 package com.sonchan.main.view
 
 import androidx.compose.runtime.Composable
+import com.sonchan.design_system.theme.SumoryTheme
+import com.sonchan.ui.DevicePreviews
 
 @Composable
 fun MainScreen(){
-    
+    SumoryTheme { colors, typography ->
+
+    }
+}
+
+@DevicePreviews
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
 }
