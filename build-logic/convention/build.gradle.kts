8fdc25dcff0fbcaab6b204fa40dede3d60179cda
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 10:13:20 +0900

    :memo: :: change build.gradle

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -8,8 +8,47 @@ java {
 }
 
 dependencies {
-    compileOnly(libs.gradle)
-    compileOnly(libs.kotlin.gradle.plugin)
-    compileOnly(libs.hilt.android.gradle.plugin)
-    compileOnly(libs.symbol.processing.gradle.plugin)
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.firebase.crashlytics.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "sumory.android.application"
+            implementationClass = "com.sumory.convention.AndroidApplicationConventionPlugin"
+        }
+
+        register("androidHilt") {
+            id = "sumory.android.hilt"
+            implementationClass = "com.sumory.convention.AndroidHiltConventionPlugin"
+        }
+
+        register("androidLint") {
+            id = "sumory.android.lint"
+            implementationClass = "com.sumory.convention.AndroidLintConventionPlugin"
+        }
+
+        register("androidCore") {
+            id = "sumory.android.core"
+            implementationClass = "com.sumory.convention.AndroidCoreConventionPlugin"
+        }
+
+        register("androidCompose") {
+            id = "sumory.android.compose"
+            implementationClass = "com.sumory.convention.AndroidComposeConventionPlugin"
+        }
+
+        register("jvmLibrary") {
+            id = "sumory.jvm.library"
+            implementationClass = "com.sumory.convention.JvmLibraryConventionPlugin"
+        }
+
+        register("androidFeature") {
+            id = "sumory.android.feature"
+            implementationClass = "com.sumory.convention.AndroidFeatureConventionPlugin"
+        }
+    }
 }
