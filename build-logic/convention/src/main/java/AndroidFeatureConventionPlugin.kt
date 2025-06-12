c00950cccba637d902bcb77cf25a4428dd3e1d78
손찬형 <sonchanhyeong710@gmail.com>
2025-06-12 09:39:38 +0900

    :sparkles: :: create AndroidFeatureConventionPlugin

---
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,39 @@
+package com.sumory.convention
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import com.sumory.convention.com.sonchan.convention.libs
+
+// Plugin class to configure Android feature modules with core, Compose, and Hilt dependencies
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+
+    // Entry point for the plugin
+    override fun apply(target: Project) {
+        with(target) {
+            // Apply necessary core, Compose, and Hilt plugins for Android
+            with(pluginManager) {
+                apply("sumory.android.core")  // Apply Compose settings from custom plugin
+                apply("sumory.android.compose")
+                apply("sumory.android.hilt")  // Apply Hilt for dependency injection
+            }
+
+            // Add necessary dependencies from Version Catalog
+            dependencies {
+                add("implementation", project(":core:design"))
+                add("implementation", project(":core:model"))
+                add("implementation", project(":core:ui"))
+
+                add("implementation", project(":data:auth"))
+                add("implementation", project(":data:diary"))
+                add("implementation", project(":domain:auth"))
+                add("implementation", project(":domain:diary"))
+
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())  // Add Lifecycle runtime for Compose
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())  // Add ViewModel support for Compose
+                add("implementation", libs.findLibrary("kotlinx.datetime").get())  // Add KotlinX DateTime library
+                add("implementation", libs.findLibrary("kotlinx.immutable").get())  // Add KotlinX Immutable collections library
+            }
+        }
+    }
+}
