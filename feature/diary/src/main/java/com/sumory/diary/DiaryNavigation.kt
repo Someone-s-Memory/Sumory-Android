964f2a5ce3d7cfcf3d8d7d182a00bee7a4cebc5c
손찬형 <sonchanhyeong710@gmail.com>
2025-06-16 19:56:00 +0900

    :sparkles: :: create Diary LazyColumn

--- feature/diary/src/main/java/com/sumory/diary/DiaryNavigation.kt
+++ feature/diary/src/main/java/com/sumory/diary/DiaryNavigation.kt
@@ -3,11 +3,25 @@ package com.sumory.diary
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.sumory.diary.view.DiaryScreen
+import com.sumory.model.diary.DiaryListEntity
 
 const val diaryRoute = "diaryRoute"
 
 fun NavGraphBuilder.diaryScreen() {
     composable(diaryRoute) {
-        DiaryScreen()
+        val dummyList = listOf(
+            DiaryListEntity(1,"즐거운 하루", "2025. 6. 10.", "😊", "☀️"),
+            DiaryListEntity(2,"비 오는 날", "2025. 6. 8.", "😐", "🌧️"),
+            DiaryListEntity(3, "행복한 순간", "2025. 6. 5.", "😄", "☀️"),
+            DiaryListEntity(1,"즐거운 하루", "2025. 6. 10.", "😊", "☀️"),
+            DiaryListEntity(2,"비 오는 날", "2025. 6. 8.", "😐", "🌧️"),
+            DiaryListEntity(3, "행복한 순간", "2025. 6. 5.", "😄", "☀️"),
+            DiaryListEntity(1,"즐거운 하루", "2025. 6. 10.", "😊", "☀️"),
+            DiaryListEntity(2,"비 오는 날", "2025. 6. 8.", "😐", "🌧️"),
+            DiaryListEntity(3, "행복한 순간", "2025. 6. 5.", "😄", "☀️"),
+        )
+        DiaryScreen(
+            diaryItems = dummyList
+        )
     }
 }
--- feature/diary/src/main/java/com/sumory/diary/view/DiaryScreen.kt
+++ feature/diary/src/main/java/com/sumory/diary/view/DiaryScreen.kt
@@ -1,12 +1,16 @@
 package com.sumory.diary.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,18 +18,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.sumory.design_system.icon.FilterIcon
 import com.sumory.design_system.theme.SumoryTheme
+import com.sumory.diary.view.component.DiaryItem
+import com.sumory.model.diary.DiaryListEntity
 import com.sumory.ui.DevicePreviews
 
 @Composable
 fun DiaryScreen(
     modifier: Modifier = Modifier,
+    diaryItems: List<DiaryListEntity>
 ){
     SumoryTheme { colors, typography ->
         Column(
             modifier
                 .fillMaxSize()
                 .background(colors.white)
-                .padding(16.dp),
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp),
         ) {
             Row(
                 modifier
@@ -43,6 +50,18 @@ fun DiaryScreen(
                     tint = colors.black
                 )
             }
+            Spacer(modifier.height(16.dp))
+            LazyColumn(
+                modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+            ) {
+                items(diaryItems){ item ->
+                    DiaryItem(
+                        item = item
+                    )
+                }
+            }
         }
     }
 }
@@ -50,5 +69,14 @@ fun DiaryScreen(
 @DevicePreviews
 @Composable
 fun DiaryScreenPreview(){
-    DiaryScreen()
+
+    val dummyList = listOf(
+        DiaryListEntity(1,"즐거운 하루", "2025. 6. 10.", "😊", "☀️"),
+        DiaryListEntity(2,"비 오는 날", "2025. 6. 8.", "😐", "🌧️"),
+        DiaryListEntity(3, "행복한 순간", "2025. 6. 5.", "😄", "☀️")
+    )
+
+    DiaryScreen(
+        diaryItems = dummyList
+    )
 }
