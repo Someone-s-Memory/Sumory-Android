6e8d898f45660c4072fec40d332392fa27cd3fdc
손찬형 <sonchanhyeong710@gmail.com>
2025-06-16 12:24:43 +0900

    :sparkles: :: add Divider

--- core/design-system/src/main/java/com/sumory/design_system/component/navigation/SumoryTopBar.kt
+++ core/design-system/src/main/java/com/sumory/design_system/component/navigation/SumoryTopBar.kt
@@ -3,13 +3,16 @@ package com.sumory.design_system.component.navigation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,27 +32,37 @@ fun SumoryTopBar(
     onNavigateTo: (String) -> Unit,
 ) {
     SumoryTheme { colors, typography ->
-        TopAppBar(
-            title = {
-                Text(text = title, style = typography.titleRegular2)
-            },
-            actions = {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(
-                        text = "💰 $coinCount",
-                        modifier = modifier
-                            .padding(start = 4.dp, end = 8.dp)
-                            .clickable { onNavigateTo("storeRoute") },
-                        style = typography.bodyRegular1
-                    )
-                    IconButton(onClick = { onNavigateTo("settingRoute") }) {
-                        SettingIcon(tint = colors.black)
+        Column {
+            TopAppBar(
+                title = {
+                    Text(text = title, style = typography.titleRegular2)
+                },
+                actions = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = "💰 $coinCount",
+                            modifier = modifier
+                                .padding(start = 4.dp, end = 8.dp)
+                                .clickable { onNavigateTo("storeRoute") },
+                            style = typography.bodyRegular1
+                        )
+                        IconButton(onClick = { onNavigateTo("settingRoute") }) {
+                            SettingIcon(tint = colors.black)
+                        }
                     }
-                }
-            }
-        )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colors.white
+                )
+            )
+            Divider(
+                color = colors.gray100,
+                thickness = 1.dp
+            )
+        }
     }
 }
+
 
 @Preview
 @Composable
