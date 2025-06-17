5c60995c1fd52425bf13184c4cb6abb801d5e769
손찬형 <sonchanhyeong710@gmail.com>
2025-06-17 18:20:43 +0900

    :memo: :: create SignInViewModel

---
+++ feature/signin/src/main/java/com/sumory/signin/viewmodel/SignInViewModel.kt
@@ -0,0 +1,10 @@
+package com.sumory.signin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignInViewModel @Inject constructor(
+): ViewModel(){
+}
