5cbd1be6b4126c9b597e4d09fff9b7754c0dc368
손찬형 <sonchanhyeong710@gmail.com>
2025-06-18 18:08:36 +0900

    :memo: :: add create RepositoryModule

---
+++ core/data/src/main/java/com/sumory/data/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.sumory.data.di
+
+import com.sumory.data.repository.auth.AuthRepository
+import com.sumory.data.repository.auth.AuthRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
