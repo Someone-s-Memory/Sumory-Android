212a3cb532099933dab99ad73d7b531a406ef459
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 10:49:45 +0900

    :memo: :: add sign-up

--- core/network/src/main/java/com/sumory/network/datasource/auth/AuthDataSource.kt
+++ core/network/src/main/java/com/sumory/network/datasource/auth/AuthDataSource.kt
@@ -1,9 +1,12 @@
 package com.sumory.network.datasource.auth
 
 import com.sumory.network.dto.auth.request.SignInRequest
+import com.sumory.network.dto.auth.request.SignUpRequest
 import com.sumory.network.dto.auth.response.SignInResponse
+import com.sumory.network.dto.auth.response.SignUpResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
     fun signIn(body: SignInRequest): Flow<SignInResponse>
+    fun signUp(body: SignUpRequest): Flow<SignUpResponse>
 }
--- core/network/src/main/java/com/sumory/network/datasource/auth/AuthDataSourceImpl.kt
+++ core/network/src/main/java/com/sumory/network/datasource/auth/AuthDataSourceImpl.kt
@@ -2,7 +2,9 @@ package com.sumory.network.datasource.auth
 
 import com.sumory.network.api.AuthApi
 import com.sumory.network.dto.auth.request.SignInRequest
+import com.sumory.network.dto.auth.request.SignUpRequest
 import com.sumory.network.dto.auth.response.SignInResponse
+import com.sumory.network.dto.auth.response.SignUpResponse
 import com.sumory.network.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -12,4 +14,7 @@ class AuthDataSourceImpl @Inject constructor(
 ): AuthDataSource{
     override fun signIn(body: SignInRequest): Flow<SignInResponse> =
         performApiRequest { api.signIn(body = body) }
+
+    override fun signUp(body: SignUpRequest): Flow<SignUpResponse> =
+        performApiRequest { api.signUp(body) }
 }
