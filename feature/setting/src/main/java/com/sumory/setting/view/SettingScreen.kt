cab9a17bc11e873615b136dd9cee224ee3ef524b
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 20:56:02 +0900

    :recycle: :: remove unusedImport

--- feature/setting/src/main/java/com/sumory/setting/view/SettingScreen.kt
+++ feature/setting/src/main/java/com/sumory/setting/view/SettingScreen.kt
@@ -1,15 +1,12 @@
 package com.sumory.setting.view
 
-import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -19,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
