daae2f202ff0e0b108eac5aec4b24ff5dee951f3
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 10:41:55 +0900

    :memo: :: create SignUpRequestMapper

---
+++ core/network/src/main/java/com/sumory/network/mapper/auth/request/SignUpRequestMapper.kt
@@ -0,0 +1,11 @@
+package com.sumory.network.mapper.auth.request
+
+import com.sumory.model.param.auth.SignUpRequestParam
+import com.sumory.network.dto.auth.request.SignUpRequest
+
+fun SignUpRequestParam.toDto(): SignUpRequest = SignUpRequest(
+    userId = this.userId,
+    password = this.password,
+    passwordCheck = this.passwordCheck,
+    nickname = this.nickname
+)
