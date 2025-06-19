60abc697cac520e6bfd3fb414e0bfe5aad7fad83
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 10:49:37 +0900

    :memo: :: add sign-up

--- core/network/src/main/java/com/sumory/network/api/AuthApi.kt
+++ core/network/src/main/java/com/sumory/network/api/AuthApi.kt
@@ -1,7 +1,9 @@
 package com.sumory.network.api
 
 import com.sumory.network.dto.auth.request.SignInRequest
+import com.sumory.network.dto.auth.request.SignUpRequest
 import com.sumory.network.dto.auth.response.SignInResponse
+import com.sumory.network.dto.auth.response.SignUpResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -10,4 +12,9 @@ interface AuthApi {
     suspend fun signIn(
         @Body body: SignInRequest
     ): SignInResponse
+
+    @POST("sign-up")
+    suspend fun signUp(
+        @Body body: SignUpRequest
+    ): SignUpResponse
 }
