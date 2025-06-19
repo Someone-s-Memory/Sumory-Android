fa2cbbd9328d6f3d9689ccd2fda1b3d2f5a65f65
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 11:05:02 +0900

    :sparkles: :: create SignUpRoute

--- feature/signup/src/main/java/com/sumory/signup/view/SignUpScreen.kt
+++ feature/signup/src/main/java/com/sumory/signup/view/SignUpScreen.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,38 +26,101 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.sumory.design_system.component.textfield.SumoryTextField
 import com.sumory.design_system.icon.EyeIcon
 import com.sumory.design_system.icon.LeftArrowIcon
 import com.sumory.design_system.theme.SumoryTheme
+import com.sumory.signup.viewmodel.SignUpViewModel
+import com.sumory.signup.viewmodel.uistate.SignUpUiState
 import com.sumory.ui.DevicePreviews
 
 @Composable
-fun SignUpScreen(
-    modifier: Modifier = Modifier,
-    onBackClick: () -> Unit
-){
+fun SignUpRoute(
+    viewModel: SignUpViewModel = hiltViewModel(),
+    onBackClick: () -> Unit,
+    onSignUpSuccess: () -> Unit
+) {
+    val signUpState by viewModel.signUpState.collectAsState()
+
     var id by remember { mutableStateOf("") }
-    var email by remember { mutableStateOf("") }
+    var nickname by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
     var checkPassword by remember { mutableStateOf("") }
     var checkPasswordVisible by remember { mutableStateOf(false) }
 
+    val isPasswordError = password != checkPassword && checkPassword.isNotEmpty()
+    val passwordErrorMessage = if (isPasswordError) "비밀번호가 일치하는지 확인해주세요" else ""
+
+    LaunchedEffect(signUpState) {
+        if (signUpState is SignUpUiState.Success) {
+            onSignUpSuccess()
+        }
+    }
+
+    SignUpScreen(
+        id = id,
+        nickname = nickname,
+        password = password,
+        passwordVisible = passwordVisible,
+        checkPassword = checkPassword,
+        checkPasswordVisible = checkPasswordVisible,
+        onIdChange = { id = it },
+        onNicknameChange = { nickname = it },
+        onPasswordChange = { password = it },
+        onPasswordVisibleChange = { passwordVisible = it },
+        onCheckPasswordChange = { checkPassword = it },
+        onCheckPasswordVisibleChange = { checkPasswordVisible = it },
+        onBackClick = onBackClick,
+        onSignUpClick = {
+            if (!isPasswordError) {
+                viewModel.signUp(
+                    userId = id,
+                    password = password,
+                    passwordCheck = checkPassword,
+                    nickname = nickname
+                )
+            }
+        },
+        isPasswordError = isPasswordError,
+        passwordErrorMessage = passwordErrorMessage
+    )
+}
+
+@Composable
+fun SignUpScreen(
+    id: String,
+    nickname: String,
+    password: String,
+    passwordVisible: Boolean,
+    checkPassword: String,
+    checkPasswordVisible: Boolean,
+    onIdChange: (String) -> Unit,
+    onNicknameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onPasswordVisibleChange: (Boolean) -> Unit,
+    onCheckPasswordChange: (String) -> Unit,
+    onCheckPasswordVisibleChange: (Boolean) -> Unit,
+    onBackClick: () -> Unit,
+    onSignUpClick: () -> Unit,
+    isPasswordError: Boolean,
+    passwordErrorMessage: String = ""
+) {
     SumoryTheme { colors, typography ->
         Box(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxSize()
                 .background(colors.white)
         ) {
             LeftArrowIcon(
-                modifier = modifier
+                modifier = Modifier
                     .padding(24.dp)
                     .clickable { onBackClick() },
                 tint = colors.black
             )
             Column(
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxSize()
                     .padding(horizontal = 32.dp),
                 verticalArrangement = Arrangement.Center,
@@ -67,30 +132,30 @@ fun SignUpScreen(
                     color = colors.black
                 )
 
-                Spacer(modifier = modifier.height(50.dp))
+                Spacer(modifier = Modifier.height(50.dp))
 
                 SumoryTextField(
-                    textState = id,
-                    placeHolder = "아이디",
-                    onTextChange = { id = it },
+                    textState = nickname,
+                    placeHolder = "닉네임",
+                    onTextChange = onNicknameChange,
                     icon = {}
                 )
 
                 SumoryTextField(
-                    textState = email,
-                    placeHolder = "이메일",
-                    onTextChange = { email = it },
+                    textState = id,
+                    placeHolder = "아이디",
+                    onTextChange = onIdChange,
                     icon = {}
                 )
 
                 SumoryTextField(
                     textState = password,
                     placeHolder = "비밀번호",
-                    onTextChange = { password = it },
+                    onTextChange = onPasswordChange,
                     icon = {
                         EyeIcon(
                             isSelected = passwordVisible,
-                            modifier = modifier.clickable { passwordVisible = !passwordVisible },
+                            modifier = Modifier.clickable { onPasswordVisibleChange(!passwordVisible) },
                             tint = colors.black
                         )
                     },
@@ -100,14 +165,14 @@ fun SignUpScreen(
                 SumoryTextField(
                     textState = checkPassword,
                     placeHolder = "비밀번호 확인",
-                    onTextChange = { checkPassword = it },
-                    isError = password != checkPassword,
-                    helperText = if(password != checkPassword) "비밀번호가 일치하는지 확인해주세요" else "",
+                    onTextChange = onCheckPasswordChange,
+                    isError = isPasswordError,
+                    helperText = passwordErrorMessage,
                     icon = {
                         EyeIcon(
                             isSelected = checkPasswordVisible,
-                            modifier = modifier.clickable {
-                                checkPasswordVisible = !checkPasswordVisible
+                            modifier = Modifier.clickable {
+                                onCheckPasswordVisibleChange(!checkPasswordVisible)
                             },
                             tint = colors.black
                         )
@@ -115,11 +180,11 @@ fun SignUpScreen(
                     visualTransformation = if (checkPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
                 )
 
-                Spacer(modifier = modifier.height(15.dp))
+                Spacer(modifier = Modifier.height(15.dp))
 
                 Button(
-                    onClick = { /* TODO: 회원가입 처리 */ },
-                    modifier = modifier
+                    onClick = onSignUpClick,
+                    modifier = Modifier
                         .fillMaxWidth()
                         .height(48.dp),
                     shape = RoundedCornerShape(12.dp),
@@ -134,8 +199,22 @@ fun SignUpScreen(
 
 @DevicePreviews
 @Composable
-fun SignUpScreenPreview(){
+fun SignUpScreenPreview() {
     SignUpScreen(
-        onBackClick = {}
+        id = "",
+        nickname = "",
+        password = "",
+        passwordVisible = false,
+        checkPassword = "",
+        checkPasswordVisible = false,
+        onIdChange = {},
+        onNicknameChange = {},
+        onPasswordChange = {},
+        onPasswordVisibleChange = {},
+        onCheckPasswordChange = {},
+        onCheckPasswordVisibleChange = {},
+        onBackClick = {},
+        onSignUpClick = {},
+        isPasswordError = false
     )
 }
