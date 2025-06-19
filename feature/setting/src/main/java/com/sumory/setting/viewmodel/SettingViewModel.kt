0dbf149fddaf3c4fcfb01e502d979a4e0dad446f
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 15:00:11 +0900

    :sparkles: :: add try catch

--- feature/setting/src/main/java/com/sumory/setting/viewmodel/SettingViewModel.kt
+++ feature/setting/src/main/java/com/sumory/setting/viewmodel/SettingViewModel.kt
@@ -3,6 +3,7 @@ package com.sumory.setting.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sumory.data.repository.auth.AuthRepository
+import com.sumory.setting.viewmodel.uistate.SignOutUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,19 +15,23 @@ class SettingViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
-    private val _signOutState = MutableStateFlow<Boolean>(false)
-    val signOutState: StateFlow<Boolean> = _signOutState
+    private val _signOutState = MutableStateFlow<SignOutUiState>(SignOutUiState.Idle)
+    val signOutState: StateFlow<SignOutUiState> = _signOutState
 
     fun signOut() {
         viewModelScope.launch {
-            authRepository.signOut()
-                .collect {
-                    _signOutState.value = true
-                }
+            try {
+                authRepository.signOut()
+                    .collect {
+                        _signOutState.value = SignOutUiState.Success
+                    }
+            } catch (e: Exception) {
+                _signOutState.value = SignOutUiState.Error(e.message ?: "로그아웃 실패")
+            }
         }
     }
 
     fun resetSignOutState() {
-        _signOutState.value = false
+        _signOutState.value = SignOutUiState.Idle
     }
 }
