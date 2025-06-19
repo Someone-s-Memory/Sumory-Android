62baa4435ba3cc572ab32cab6c22c22a1089220b
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 09:20:13 +0900

    :memo: :: create AuthInterceptor

---
+++ core/network/src/main/java/com/sumory/network/util/AuthInterceptor.kt
@@ -0,0 +1,20 @@
+package com.sumory.network.util
+
+import com.sumory.data.datastore.auth.TokenDataStore
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(
+    private val tokenDataStore: TokenDataStore
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = runBlocking { tokenDataStore.accessToken.firstOrNull() }
+        val request = chain.request().newBuilder()
+        token?.let {
+            request.addHeader("Authorization", "Bearer $it")
+        }
+        return chain.proceed(request.build())
+    }
+}
