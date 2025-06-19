9b6dd39ae3eec9eb1b788e7321a23cf33ae8bfb6
손찬형 <sonchanhyeong710@gmail.com>
2025-06-19 10:42:21 +0900

    :memo: :: create SignUpRequestParam

---
+++ core/model/src/main/java/com/sumory/model/param/auth/SignUpRequestParam.kt
@@ -0,0 +1,8 @@
+package com.sumory.model.param.auth
+
+data class SignUpRequestParam(
+    val userId: String,
+    val password: String,
+    val passwordCheck: String,
+    val nickname: String
+)
