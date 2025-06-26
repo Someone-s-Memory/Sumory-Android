f35c0bb5c7836056b76f0d2370cfd6d823aaf902
손찬형 <sonchanhyeong710@gmail.com>
2025-06-26 09:54:50 +0900

    :memo: :: add date

--- core/model/src/main/java/com/sumory/model/entity/calendar/CalendarDiaryListEntity.kt
+++ core/model/src/main/java/com/sumory/model/entity/calendar/CalendarDiaryListEntity.kt
@@ -1,9 +1,16 @@
 package com.sumory.model.entity.calendar
 
+import java.time.LocalDate
+
 data class CalendarDiaryListEntity(
     val id: Int,
     val title: String,
     val content: String,
     val emotionEmoji: String,
-    val weatherEmoji: String
+    val weatherEmoji: String,
+    val date: LocalDate? = null
 )
+
+fun CalendarDiaryListEntity.withDate(date: LocalDate): CalendarDiaryListEntity {
+    return this.copy(date = date)
+}
