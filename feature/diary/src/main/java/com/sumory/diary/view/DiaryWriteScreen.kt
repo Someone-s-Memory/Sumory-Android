89eea2b357eff17798b44acc8b4a2e4c56f759e3
손찬형 <sonchanhyeong710@gmail.com>
2025-06-30 13:44:42 +0900

    :sparkles: :: create DiaryWriteScreen

--- feature/diary/src/main/java/com/sumory/diary/view/DiaryWriteScreen.kt
+++ feature/diary/src/main/java/com/sumory/diary/view/DiaryWriteScreen.kt
@@ -1,28 +1,58 @@
 package com.sumory.diary.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sumory.design_system.icon.LeftArrowIcon
 import com.sumory.design_system.theme.SumoryTheme
 import com.sumory.ui.DevicePreviews
 
 @Composable
-fun DiaryWriteScreen(){
+fun DiaryWriteScreen(
+    modifier: Modifier = Modifier,
+    date: String,
+    onBackClick: () -> Unit
+){
     SumoryTheme { colors, typography ->
-        Box(
-            Modifier
+        Column(
+            modifier = modifier
                 .fillMaxSize()
-                .background(colors.white),
-            contentAlignment = Alignment.Center
+                .background(colors.white)
+                .padding(16.dp)
+                .padding(WindowInsets.systemBars.asPaddingValues())
         ) {
-            Text(
-                text = "일기 작성 화면",
-                color = colors.black
-            )
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                LeftArrowIcon(
+                    modifier = modifier
+                        .clickable { onBackClick() },
+                    tint = colors.black
+                )
+                Spacer(modifier.weight(1f))
+                Text(
+                    text = date,
+                    style = typography.titleBold3,
+                    color = colors.black
+                )
+                Spacer(modifier.weight(1f))
+                Spacer(modifier.width(24.dp))
+            }
         }
     }
 }
@@ -30,5 +60,8 @@ fun DiaryWriteScreen(){
 @DevicePreviews
 @Composable
 private fun DiaryWriteScreenPreview(){
-    DiaryWriteScreen()
+    DiaryWriteScreen(
+        date = "2025년 7월 10일 수요일",
+        onBackClick = {}
+    )
 }
