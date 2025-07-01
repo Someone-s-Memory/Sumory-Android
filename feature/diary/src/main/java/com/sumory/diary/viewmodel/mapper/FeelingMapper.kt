d9cd7de416768e58d49bac6915698dd8641132d6
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 15:01:35 +0900

    :sparkles: :: create FeelingMapper

---
+++ feature/diary/src/main/java/com/sumory/diary/viewmodel/mapper/FeelingMapper.kt
@@ -0,0 +1,14 @@
+package com.sumory.diary.viewmodel.mapper
+
+import androidx.annotation.DrawableRes
+import com.sumory.design_system.R
+import com.sumory.model.type.DiaryFeeling
+
+@DrawableRes
+fun DiaryFeeling.iconRes(): Int = when (this) {
+    DiaryFeeling.HAPPY -> R.drawable.ic_coin
+    DiaryFeeling.SAD -> R.drawable.ic_coin
+    DiaryFeeling.ANGRY -> R.drawable.ic_coin
+    DiaryFeeling.FUN -> R.drawable.ic_coin
+    DiaryFeeling.THINKING -> R.drawable.ic_coin
+}
