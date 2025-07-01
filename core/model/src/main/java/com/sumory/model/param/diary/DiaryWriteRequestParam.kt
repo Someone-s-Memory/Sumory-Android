5121b89d2ff75606ba0d1c3689d8afbc3b0cc5aa
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:08:27 +0900

    :sparkles: :: create DiaryWriteRequestParam

---
+++ core/model/src/main/java/com/sumory/model/param/diary/DiaryWriteRequestParam.kt
@@ -0,0 +1,10 @@
+package com.sumory.model.param.diary
+
+data class DiaryWriteRequestParam (
+    val title: String,
+    val content: String,
+    val feeling: String,
+    val weather: String,
+    val date: String,
+    val picture: String
+)
