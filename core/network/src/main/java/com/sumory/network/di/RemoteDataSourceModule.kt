7a204c56e2e97cbb7787fc05993dd5f390f72384
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 11:18:43 +0900

    :memo: :: add bindDiaryDataSource

--- core/network/src/main/java/com/sumory/network/di/RemoteDataSourceModule.kt
+++ core/network/src/main/java/com/sumory/network/di/RemoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.sumory.network.di
 
 import com.sumory.network.datasource.auth.AuthDataSource
 import com.sumory.network.datasource.auth.AuthDataSourceImpl
+import com.sumory.network.datasource.diary.DiaryDataSource
+import com.sumory.network.datasource.diary.DiaryDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,4 +20,10 @@ abstract class RemoteDataSourceModule {
     abstract fun bindAuthDataSource(
         authDatasourceImpl: AuthDataSourceImpl
     ): AuthDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindDiaryDataSource(
+        diaryDatasourceImpl: DiaryDataSourceImpl
+    ): DiaryDataSource
 }
