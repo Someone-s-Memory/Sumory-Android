c38319fcc3222be2c0f70fc84cb68dc634a8a335
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:21:42 +0900

    :sparkles: :: create DiaryDataSourceImpl

---
+++ core/network/src/main/java/com/sumory/network/datasource/diary/DiaryDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.sumory.network.datasource.diary
+
+import com.sumory.network.api.DiaryApi
+import com.sumory.network.dto.diary.request.DiaryWriteRequest
+import com.sumory.network.dto.diary.response.DiaryWriteResponse
+import com.sumory.network.util.performApiRequest
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class DiaryDataSourceImpl @Inject constructor(
+    private val api: DiaryApi
+): DiaryDataSource {
+    override fun diaryWrite (body: DiaryWriteRequest): Flow<DiaryWriteResponse> =
+        performApiRequest { api.diaryWrite(body = body) }
+}
