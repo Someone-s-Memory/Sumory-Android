c3e85fcbdb7fea7f3ea7a57da2a3817981eb81e2
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 15:01:46 +0900

    :sparkles: :: create WeatherMapper

---
+++ feature/diary/src/main/java/com/sumory/diary/viewmodel/mapper/WeatherMapper.kt
@@ -0,0 +1,14 @@
+package com.sumory.diary.viewmodel.mapper
+
+import androidx.annotation.DrawableRes
+import com.sumory.design_system.R
+import com.sumory.model.type.DiaryWeather
+
+@DrawableRes
+fun DiaryWeather.iconRes(): Int = when (this) {
+    DiaryWeather.SUNNY -> R.drawable.ic_coin
+    DiaryWeather.CLOUDY ->  R.drawable.ic_coin
+    DiaryWeather.RAINY ->  R.drawable.ic_coin
+    DiaryWeather.SNOWY ->  R.drawable.ic_coin
+    DiaryWeather.THUNDER ->  R.drawable.ic_coin
+}
