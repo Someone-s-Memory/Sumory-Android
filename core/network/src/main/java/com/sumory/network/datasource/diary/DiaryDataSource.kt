f5b0b4f5e8d384afce6893c48bd60bcb4400f090
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:21:31 +0900

    :sparkles: :: create DiaryDataSource

---
+++ core/network/src/main/java/com/sumory/network/datasource/diary/DiaryDataSource.kt
@@ -0,0 +1,9 @@
+package com.sumory.network.datasource.diary
+
+import com.sumory.network.dto.diary.request.DiaryWriteRequest
+import com.sumory.network.dto.diary.response.DiaryWriteResponse
+import kotlinx.coroutines.flow.Flow
+
+interface DiaryDataSource {
+    fun diaryWrite(body: DiaryWriteRequest): Flow<DiaryWriteResponse>
+}
