bb3c392d34fa1513de80ea53f44227c2eb46b67c
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:00:41 +0900

    :sparkles: :: create DiaryWriteResponse

--- core/network/src/main/java/com/sumory/network/dto/diary/response/DiaryWriteResponse.kt
+++ core/network/src/main/java/com/sumory/network/dto/diary/response/DiaryWriteResponse.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class DiaryWriteResponse(
-    @Json(name = "message") val success: String,
+    @Json(name = "message") val message: String,
 )
