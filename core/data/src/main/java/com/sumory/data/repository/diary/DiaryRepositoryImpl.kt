9dc6f497f19526d6dad7ba608312f65af284dad1
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:25:42 +0900

    :sparkles: :: create DiaryRepositoryImpl

---
+++ core/data/src/main/java/com/sumory/data/repository/diary/DiaryRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.sumory.data.repository.diary
+
+import com.sumory.model.model.diary.DiaryWriteResponseModel
+import com.sumory.model.param.diary.DiaryWriteRequestParam
+import com.sumory.network.datasource.diary.DiaryDataSource
+import com.sumory.network.mapper.diary.request.toDto
+import com.sumory.network.mapper.diary.response.toModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
+import javax.inject.Inject
+
+class DiaryRepositoryImpl @Inject constructor(
+    private val diaryDataSource: DiaryDataSource
+) : DiaryRepository {
+
+    override suspend fun diaryWrite(body: DiaryWriteRequestParam): Flow<DiaryWriteResponseModel> {
+        return diaryDataSource.diaryWrite(body.toDto()).transform {
+            emit(it.toModel())
+        }
+    }
+}
