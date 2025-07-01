9a229db8f7943e7dc7cff3e08f6bc5f8121a7431
손찬형 <sonchanhyeong710@gmail.com>
2025-07-01 10:12:01 +0900

    :sparkles: :: create DiaryWriteRequestMapper

---
+++ core/network/src/main/java/com/sumory/network/mapper/diary/request/DiaryWriteRequestMapper.kt
@@ -0,0 +1,14 @@
+package com.sumory.network.mapper.diary.request
+
+import com.sumory.model.param.diary.DiaryWriteRequestParam
+import com.sumory.network.dto.diary.request.DiaryWriteRequest
+
+fun DiaryWriteRequestParam.toDto(): DiaryWriteRequest =
+    DiaryWriteRequest(
+        title = this.title,
+        content = this.content,
+        feeling = this.feeling,
+        weather = this.weather,
+        date = this.date,
+        picture = this.picture
+    )
