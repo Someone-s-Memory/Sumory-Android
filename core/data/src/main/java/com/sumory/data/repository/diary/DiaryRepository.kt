aec433f8a46826f7a76eb5dfaed9be3593cd2471
손찬형 <sonchanhyeong710@gmail.com>
2025-07-04 09:37:29 +0900

    :memo: :: add updateDiary

--- core/data/src/main/java/com/sumory/data/repository/diary/DiaryRepository.kt
+++ core/data/src/main/java/com/sumory/data/repository/diary/DiaryRepository.kt
@@ -6,6 +6,8 @@ import com.sumory.model.model.diary.DiaryDeleteResponseModel
 import com.sumory.model.model.diary.DiaryDetailResponseModel
 import com.sumory.model.model.diary.DiaryWriteResponseModel
 import com.sumory.model.param.diary.DiaryWriteRequestParam
+import com.sumory.network.dto.diary.request.DiaryUpdateRequest
+import com.sumory.network.dto.diary.response.DiaryUpdateResponse
 import kotlinx.coroutines.flow.Flow
 
 interface DiaryRepository {
@@ -14,5 +16,6 @@ interface DiaryRepository {
     suspend fun getAllDiary(forceRefresh: Boolean = false): Flow<List<AllDiaryResponseModel>>
     suspend fun getDiaryDetail(diaryId: Int): DiaryDetailResponseModel
     suspend fun deleteDiary(date: String, title: String): Flow<DiaryDeleteResponseModel>
+    suspend fun updateDiary(body: DiaryUpdateRequest): Flow<DiaryUpdateResponse>
     suspend fun clearAllCache()
 }
