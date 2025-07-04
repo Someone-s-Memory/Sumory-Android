1bc32c52c25e0abc32660439850d61dccb2eea7d
손찬형 <sonchanhyeong710@gmail.com>
2025-07-04 09:35:32 +0900

    :memo: :: add updateDiary

--- core/network/src/main/java/com/sumory/network/datasource/diary/DiaryDataSource.kt
+++ core/network/src/main/java/com/sumory/network/datasource/diary/DiaryDataSource.kt
@@ -1,10 +1,12 @@
 package com.sumory.network.datasource.diary
 
+import com.sumory.network.dto.diary.request.DiaryUpdateRequest
 import com.sumory.network.dto.diary.request.DiaryWriteRequest
 import com.sumory.network.dto.diary.response.AllDiaryResponse
 import com.sumory.network.dto.diary.response.DateDiaryResponse
 import com.sumory.network.dto.diary.response.DiaryDeleteResponse
 import com.sumory.network.dto.diary.response.DiaryDetailResponse
+import com.sumory.network.dto.diary.response.DiaryUpdateResponse
 import com.sumory.network.dto.diary.response.DiaryWriteResponse
 import kotlinx.coroutines.flow.Flow
 
@@ -14,4 +16,5 @@ interface DiaryDataSource {
     fun getAllDiary(): Flow<List<AllDiaryResponse>>
     fun getDiaryDetail(diaryId: Int): Flow<DiaryDetailResponse>
     fun deleteDiary(date: String, title: String): Flow<DiaryDeleteResponse>
+    fun updateDiary(body: DiaryUpdateRequest): Flow<DiaryUpdateResponse>
 }
