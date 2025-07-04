cb0f36216d069cc52c32fdd4a62b51595d3b9080
손찬형 <sonchanhyeong710@gmail.com>
2025-07-04 09:30:54 +0900

    :sparkles: :: create DiaryUpdateRequest

---
+++ core/network/src/main/java/com/sumory/network/dto/diary/request/DiaryUpdateRequest.kt
@@ -0,0 +1,15 @@
+package com.sumory.network.dto.diary.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class DiaryUpdateRequest(
+    @Json(name = "title") val title: String,
+    @Json(name = "content") val content: String,
+    @Json(name = "feeling") val feeling: String,
+    @Json(name = "weather") val weather: String,
+    @Json(name = "date") val date: String,
+    @Json(name = "pictures") val pictures: List<String>,
+    @Json(name = "change") val change: String
+)
