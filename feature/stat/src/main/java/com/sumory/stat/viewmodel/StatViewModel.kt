d59f09aa4580f4f16a4c834f235ac25a28a1633a
손찬형 <sonchanhyeong710@gmail.com>
2025-08-28 08:55:11 +0900

    :sparkles: :: create StatViewModel

---
+++ feature/stat/src/main/java/com/sumory/stat/viewmodel/StatViewModel.kt
@@ -0,0 +1,11 @@
+package com.sumory.stat.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StatViewModel @Inject constructor(
+
+) : ViewModel() {
+}
